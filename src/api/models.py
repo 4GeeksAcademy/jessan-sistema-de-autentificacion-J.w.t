e3f3d684ef01a4eee146296cdbe3315f1719007a
Jessan23 <jessicaanairayagrez@gmail.com>
2024-12-17 03:32:57 +0000

    genere un token

--- src/api/models.py
+++ src/api/models.py
@@ -6,6 +6,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    name = db.Column(db. String(120), unique=False, nullable=False)
+    lastname = db.Column(db. String(120), unique=False, nullable=False)
+    age = db.Column(db. String(120), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,9 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity
+
+
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +23,19 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+
+
+@api.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+
+
+
