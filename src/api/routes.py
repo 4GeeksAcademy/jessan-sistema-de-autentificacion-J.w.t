6bdbfac20ad8e0ad1c77f5377c03bfee06800e9a
Jessan23 <jessicaanairayagrez@gmail.com>
2024-12-19 06:41:10 +0000

    entenderle al token

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,7 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from flask_jwt_extended import create_access_token, get_jwt_identity
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
 
 
@@ -24,18 +24,35 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-
+#----------------------------------------------------------------------------------------------------------
 
 
 @api.route("/login", methods=["POST"])
 def login():
-    username = request.json.get("username", None)
+    email = request.json.get("email", None)
     password = request.json.get("password", None)
-    if username != "test" or password != "test":
+    name = request.json.get("name", None)
+    lastname = request.json.get("lastname", None)
+    age = request.json.get("age", None)
+
+
+    user= User.query.filter_by(email=email, password=password, name=name, lastname=lastname, age=age ).first()
+
+    if user is None :
         return jsonify({"msg": "Bad username or password"}), 401
 
-    access_token = create_access_token(identity=username)
+    access_token = create_access_token(identity=email)
     return jsonify(access_token=access_token)
+
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
 
 
 
